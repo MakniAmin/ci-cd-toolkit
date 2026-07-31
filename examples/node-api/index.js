3d77df73abb5e7c50ac44771f30f587a5f65b4c1
MakniAmin <amin.makni@outlook.fr>
2026-07-31 11:05:48 +0100

    trigger first pipeline run

--- examples/node-api/index.js
+++ examples/node-api/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.json({ status: 'ok', service: 'node-api' });
+  res.json({ status: 'healthy', service: 'node-api' });
 });
 
 app.get('/health', (req, res) => res.sendStatus(200));
